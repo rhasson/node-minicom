aee5126c84ab5e58214bfc34bf45f55fd6adf858
Roy Hasson <rhasson@gmail.com>
2012-11-15 12:14:24 -0500

    simple state machine

--- examples/modem_repl.js
+++ examples/modem_repl.js
@@ -10,15 +10,24 @@ var mc = new Mini(),
 	port1.modem.on('data', function(data) {
 		console.log('Port1 data: ', data);
 	});
+	port1.modem.on('call', function(data) {
+		console.log('Port1 call: ', data);
+	});
 
 	port2 = mc.addPort({
-                 port: '/dev/ttyACM2',
-                 phone: '14444447003'
-        });
+  	port: '/dev/ttyACM2',
+    phone: '14444447003'
+  });
+	
 	console.log('Connected on port: ', port2.port, 'with : ', port2.phone);
-        port2.modem.on('data', function(data) {
-                console.log('Port2 data: ', data);
-		if (data.data.code === 'RING') port2.modem.write('ATA');
-        });
+  
+  port2.modem.on('data', function(data) {
+  	console.log('Port2 data: ', data);
+		//if (data.data.code === 'RING') port2.modem.write('ATA');
+  });
+  port2.modem.on('call', function(data) {
+  	console.log('Port2 call: ', data);
+		//if (data.data.code === 'RING') port2.modem.write('ATA');
+  });
 
 	repl.start({prompt: '=>'}).context.ports = {p1: port1, p2:port2}
